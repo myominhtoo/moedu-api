e6cb156d9b133e6d7859cc52e8d0c75f23ffc079
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-12-08 15:33:26 +0630

    project inited

---
+++ src/test/java/com/lio/moeduhouseapi/MoeduHouseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lio.moeduhouseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoeduHouseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
